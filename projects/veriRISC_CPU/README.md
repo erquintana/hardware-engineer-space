10daecf84d2aed96cba3428c277a8f091d2fd36a
Esteban RQ <erquintana0805@gmail.com>
2023-07-28 23:07:24 -0600

    adding main file for uart project

---
+++ projects/veriRISC_CPU/README.md
@@ -0,0 +1,24 @@
+## Project Title: Verifying the VeriRISC CPU
+
+## Project Description:
+<>
+## Module Specifications:
+
+### VeriRISC CPU Module:
+<>
+#### Parameters:
+- <>
+
+
+#### Inputs:
+- <> : <>
+
+
+#### Outputs:
+- <> : <>
+
+## Explanation of URF Logic:
+- <>
+
+## Testbench Module:
+<>
---
+++ projects/veriRISC_CPU/veriRISC_CPU.sv
@@ -0,0 +1,13 @@
+//------------------------------------------------------------------------------
+// Project: Verifying the VeriRISC CPU
+// File:    veriRISC_CPU.sv
+// Author:  Esteban Rodríguez Quintana
+// Date:    <>
+//
+// Description: <>
+//
+// Revision History:
+// - <Date>: <Version / Modification Description>
+//
+//------------------------------------------------------------------------------
+
