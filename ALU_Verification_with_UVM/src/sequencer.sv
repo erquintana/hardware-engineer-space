e968ae08d247e0eb1f02351a79c2fabe2462b688
erquintana <42876141+erquintana@users.noreply.github.com>
2023-08-04 14:32:11 +0000

    deleting, movin and cleaning

--- ALU_Verification_with_UVM/src/sequencer.sv
+++
@@ -1,9 +0,0 @@
-class sequencer extends uvm_sequencer #(alu_seq_packet);
-    `uvm_component_utils(sequencer);
-
-    // Class constructor:
-    function new(string name = "sequencer", uvm_component parent = null);
-        super.new(name, parent);
-        `uvm_info(this.get_name(),"sequencer instance created", UVM_MEDIUM);
-    endfunction
-endclass
